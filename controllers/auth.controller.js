50986ddf734cb46618698f69df35405e34f54c01
FThappy <thangchu766@gmail.com>
2024-06-07 12:58:26 +0700

    new

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -114,6 +114,7 @@ export const login = async (req, res) => {
       .cookie("Authorization", accessToken, {
         httpOnly: true,
         maxAge: age,
+        secure: true, // https
         sameSite: "None",
       })
       .status(200)
