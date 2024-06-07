bc4f44c27ea59f57ab7739070ddc33f48ab9e9fb
FThappy <thangchu766@gmail.com>
2024-06-07 13:55:16 +0700

    new

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ mongoose
 app.use(
   cors({
     // origin: "http://localhost:3000",
-    origin: "https://datn-fe-omega.vercel.app",
+    origin: "https://datn-fe-two.vercel.app",
     credentials: true,
   })
 );
@@ -42,7 +42,7 @@ const io = new Server(server, {
   cookie: true,
   cors: {
     // origin: "http://localhost:3000",
-    origin: "https://datn-fe-omega.vercel.app",
+    origin: "https://datn-fe-two.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   },
